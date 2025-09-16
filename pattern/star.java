3517b0ea60a32ae044149dee7d6c5fdf48903d04
Jagannath Nahak <jagannathnahak551@gmail.com>
2025-09-16 21:03:10 +0530

    dsa

---
+++ pattern/star.java
@@ -0,0 +1,9 @@
+package pattern;
+public class star {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    } 
+}
