94331724e81a1461e57f09337dd14d0321159be5
Jagannath Nahak <jagannathnahak551@gmail.com>
2025-09-16 21:16:35 +0530

    printtable

---
+++ pattern/multipication.java
@@ -0,0 +1,10 @@
+package pattern;
+
+public class multipication {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.println("10"+"*"+i+"="+i*10);
+        }
+        System.out.println();
+    } 
+}
