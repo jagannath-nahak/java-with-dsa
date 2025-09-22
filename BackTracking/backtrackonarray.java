1ef544e6d6cb67c8c55d963466e20f3d3cd0b1ab
Jagannath Nahak <jagannathnahak551@gmail.com>
2025-09-22 16:38:05 +0530

    backtrack-array

---
+++ BackTracking/backtrackonarray.java
@@ -0,0 +1,28 @@
+package BackTracking;
+
+public class backtrackonarray {
+    public static void changeArray(int arr[],int i,int val){
+        // base case
+        if(i==arr.length){
+            printArray(arr);
+            return;
+        }
+        arr[i]=val;
+        // it is the normal function call
+        changeArray(arr, i+1, val+1);
+        // it is the recursion call
+        arr[i]=val-2;
+    }
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        changeArray(arr,0,1);
+        printArray(arr);
+    }
+    
+}
